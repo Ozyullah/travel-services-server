53e613a2672eef9d40299afc02a337c26e7ff9bf
habibullah <habibullahozy298796@gmail.com>
2023-01-10 21:22:47 +0600

    added delete

--- index.js
+++ index.js
@@ -84,7 +84,12 @@ async function run(){
             res.send(desires)
         })
 
-        
+        app.delete('reviews/:id', async(req, res)=>{
+            const id =req.params.id;
+            const query ={_id: ObjectId(id)};
+            const result=await reviewCollections.deleteOne(query);
+            res.send(result)
+        })
  
     }
     finally{
